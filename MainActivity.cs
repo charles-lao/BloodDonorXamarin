577d04dd8dfa9f8e4b265db57119ab62bb9a8215
charles-lao <charles.h.lao@gmail.com>
2025-06-21 23:44:28 +1200

    Complete Lesson 107: Bind Donor's Data to RecyclerView

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,18 +1,48 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.Widget;
 using AndroidX.AppCompat.App;
+using BloodDonorXamarin.Adapters;
+using BloodDonorXamarin.DataModels;
+using System.Collections.Generic;
 
 namespace BloodDonorXamarin
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        RecyclerView donorsRecyclerView;
+        DonorsAdapter donorsAdapter;
+        List<Donor> listOfDonors;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.donor_row);
+            SetContentView(Resource.Layout.activity_main);
+
+            donorsRecyclerView = (RecyclerView)FindViewById(Resource.Id.donorsRecyclerView);
+            CreateData();
+            SetupRecyclerView();
+        }
+
+        void CreateData()
+        {
+            listOfDonors = new List<Donor>();
+            listOfDonors.Add(new Donor { BloodGroup = "AB+", City = "Delaware", Country = "USA", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+            listOfDonors.Add(new Donor { BloodGroup = "O+", City = "Munich", Country = "Germany", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+            listOfDonors.Add(new Donor { BloodGroup = "O-", City = "Lagos", Country = "Nigeria", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+            listOfDonors.Add(new Donor { BloodGroup = "AB+", City = "North Carolina", Country = "USA", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+            listOfDonors.Add(new Donor { BloodGroup = "O+", City = "Madrid", Country = "Spain", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+            listOfDonors.Add(new Donor { BloodGroup = "AB+", City = "New York", Country = "USA", Email = "ufinixacademy@gmail.com", Fullname = "Ufinix Academy", Phone = "+01 76376 883" });
+        }
+
+        void SetupRecyclerView()
+        {
+            donorsRecyclerView.SetLayoutManager(new LinearLayoutManager(donorsRecyclerView.Context));
+            donorsAdapter = new DonorsAdapter(listOfDonors);
+            donorsRecyclerView.SetAdapter(donorsAdapter);
         }
     }
 }
