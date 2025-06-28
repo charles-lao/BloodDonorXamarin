845abce866a89d1d93be1831229b674059bdbb8a
charles-lao <charles.h.lao@gmail.com>
2025-06-28 23:01:42 +1200

    Complete Lesson 115: Set up Material Spinner

--- Fragments/NewDonorFragment.cs
+++ Fragments/NewDonorFragment.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using FR.Ganfra.Materialspinner;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,11 @@ namespace BloodDonorXamarin.Fragments
 {
     public class NewDonorFragment : AndroidX.Fragment.App.DialogFragment
     {
+        MaterialSpinner materialSpinner;
+        List<string> bloodGroupsList;
+        ArrayAdapter<string> spinnerAdapter;
+        string selectedBloodGroup;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,8 +33,38 @@ namespace BloodDonorXamarin.Fragments
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
             View view = inflater.Inflate(Resource.Layout.addnew, container, false);
-
+            materialSpinner = (MaterialSpinner)view.FindViewById(Resource.Id.materialSpinner);
+            SetupSpinner();
             return view;
+            
+        }
+
+        void SetupSpinner()
+        {
+            bloodGroupsList = new List<string>();
+            bloodGroupsList.Add("O+");
+            bloodGroupsList.Add("O-");
+            bloodGroupsList.Add("AB+");
+            bloodGroupsList.Add("AB-");
+            bloodGroupsList.Add("A+");
+            bloodGroupsList.Add("A-");
+            bloodGroupsList.Add("B+");
+            bloodGroupsList.Add("B-");
+
+            spinnerAdapter = new ArrayAdapter<string>(Activity, Android.Resource.Layout.SimpleSpinnerDropDownItem, bloodGroupsList);
+            spinnerAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            materialSpinner.Adapter = spinnerAdapter;
+            materialSpinner.ItemSelected += MaterialSpinner_ItemSelected;
+        }
+
+        private void MaterialSpinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            if (e.Position != -1)
+            {
+                selectedBloodGroup = bloodGroupsList[e.Position];
+                Console.WriteLine(selectedBloodGroup);
+            }
             
         }
     }
